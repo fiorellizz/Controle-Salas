a3982d0a2fc616c4563c6444c03625d83e7b11a1
Caixetaa_ <guilhermecaixeta19@gmail.com>
2025-01-16 14:07:48 -0300

    Delete usuarios directory

--- usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views  
-urlpatterns = [
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('logar/', views.login, name='login'),
-    path('sair/', views.sair, name='sair'),
-    path('trocar/', views.cadastro, name='trocar'),
-]
